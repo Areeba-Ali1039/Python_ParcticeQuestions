74f52e6f143919eed7b988711e58132fab22a94b
Areeba-Ali <areebaali1039@gmail.com>
2026-08-24 00:17:47 +0500

    Add files via upload

---
+++ Chap2_Inputftn.py
@@ -0,0 +1,41 @@
+#input() ftn
+# a = int(input("Enter a number: "))
+# b = int(input ("Enter a number: "))
+# 
+# print ("The value of a is: ",a)
+# 
+# print ("The value of b is: ",b)
+# 
+# print("Sum: ",a+b)
+
+# Quest 1
+a = int(input("Enter the value of a"))
+b = int(input("Enter the value of b"))
+#print("Sum: ",a+b)
+
+# Quest 2 --> To find the remainder 
+
+c = a%b
+print ("Remainder: ", c)
+
+# Quest 3 
+# k = "Areeb"
+# z= type(k)
+# print(z)
+# 
+#Quest 4 
+# a = 34
+# b=80
+# print(a>b)
+
+#quest 5
+# a=int(input("Enter a number a"))
+# b=int(input("Enter a number b"))
+# 
+# avg = (a+b)/2
+# print ("Average: ",avg)
+# 
+#Quest 6
+# z =int(input("Enter a number: "))
+# sqr = z*z # or a**2
+# print("SQuare of a number: ",sqr)
