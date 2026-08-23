ad429c30afd4ceaf20744ccdbbbd71ae9152dd27
Areeba-Ali <areebaali1039@gmail.com>
2026-08-24 00:19:56 +0500

    Add files via upload

---
+++ Chap4_PractQues.py
@@ -0,0 +1,40 @@
+# furits = []
+# 
+# f1 = input("Enter a fruit name: ")
+# furits.append(f1)
+# f2 = input("Enter a fruit name: ")
+# furits.append(f2)
+# f3 = input("Enter a fruit name: ")
+# furits.append(f3)
+# f4 = input("Enter a fruit name: ")
+# furits.append(f4)
+# f5 = input("Enter a fruit name: ")
+# furits.append(f5)
+# f6 = input("Enter a fruit name: ")
+# furits.append(f6)
+# print(furits)
+#Q2
+# marks=[]
+# m1 = int(input("Enter your marks : "))
+# marks.append(m1)
+# m2 = int(input("Enter your marks : "))
+# marks.append(m2)
+# m3 = int(input("Enter your marks : "))
+# marks.append(m3)
+# m4 = int(input("Enter your marks : "))
+# marks.append(m4)
+# m5 = int(input("Enter your marks : "))
+# marks.append(m5)
+# m6 = int(input("Enter your marks : "))
+# marks.append(m6)
+# marks.sort()
+#print(marks)
+#Q3
+# tuple = (2,56,78.9,"srt")
+# tuple[2] = "Aree"
+#Q4
+# list=[4,5,2,1]
+# print (sum(list))
+#Q 5
+a = (7, 0, 8, 0, 0, 9)
+print(a.count(0))
