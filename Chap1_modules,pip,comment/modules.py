802ab57a83199cc0ea23c59f2c3c240019291712
Areeba-Ali <areebaali1039@gmail.com>
2026-08-24 00:13:34 +0500

    Add files via upload

---
+++ Chap1_modules,pip,comment/modules.py
@@ -0,0 +1,16 @@
+# import pyjokes 
+# 
+# joke = pyjokes.get_joke()
+# 
+# print (joke)
+# single line comment contr+/
+# """ multiple 
+# line 
+# comment """
+
+# pyttsx3 module --> text to speech 
+import pyttsx3
+engine = pyttsx3.init()
+engine.say('''Hello i am Munaza ali . i have completed my FSC and now i am free 
+in home , doing nothing just wasting my time . munaza ali is a good girl . she is beautiful like moon.the person i love the most is my mom''')
+engine.runAndWait()
